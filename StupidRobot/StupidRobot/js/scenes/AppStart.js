addb28e70d3011f0c6ec4c758c77545ac30593c8
Timo Frank <Timo-Frank@gmx.net>
2013-10-07 23:54:01 +0200

    put each scene in an own file

--- StupidRobot/StupidRobot/js/scenes/AppStart.js
+++ StupidRobot/StupidRobot/js/scenes/AppStart.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="/libs/crafty.js"/>
-/// <reference path="game.js"/>
-/// <reference path="components.js"/>
-
-Crafty.scene('AppStart', function () {
+﻿Crafty.scene('AppStart', function () {
 	Crafty.background('black');
 
 	Crafty.e('2D,Canvas,Text')
@@ -23,29 +19,4 @@ Crafty.scene('AppStart', function () {
 	], function() {
 		Crafty.scene('MainMenu');
 	});
-});
-
-Crafty.scene('MainMenu', function () {
-	Crafty.background('white');
-
-	Crafty.e('2D,Canvas,Button')
-		.Button('Start')
-		.attr({
-			x: 50,
-			y: 500
-		});
-
-	Crafty.e('2D,Canvas,Button')
-		.Button('Settings')
-		.attr({
-			x: 50,
-			y: 580
-		});
-
-	Crafty.e('2D,Canvas,Button')
-		.Button('Help')
-		.attr({
-			x: 50,
-			y: 660
-		});
 });
