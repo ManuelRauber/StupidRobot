5eacf9eab3ed6fc8f27749a80acd3be997c089cc
Timo Frank <Timo-Frank@gmx.net>
2013-10-08 13:52:37 +0200

    added simple mainmenu button animation

--- StupidRobot/StupidRobot/js/scenes/MainMenu.js
+++ StupidRobot/StupidRobot/js/scenes/MainMenu.js
@@ -1,38 +1,86 @@
 ﻿Crafty.scene('MainMenu', function () {
     Crafty.background('white');
 
-    Crafty.e('2D,Canvas,Button')
+    //Campaign button
+    Crafty.e('2D,Canvas,Button,Tween')
 		.Button('Campaign')
 		.attr({
-		    x: 50,
-		    y: 50
-		});
+		    x: -250,
+		    y: 50,
+		})
+        .bind('startMenuAnimation', function () {
+            this.tween({
+                x: 50
+            }, 10).bind('TweenEnd', function () {
+                this.unbind('TweenEnd');
+                Crafty.trigger('menuanimation1');
+            })
+        });
 
-    Crafty.e('2D,Canvas,Button')
+
+    //Play Map button
+    Crafty.e('2D,Canvas,Button,Tween')
 		.Button('Play map')
 		.attr({
-		    x: 50,
+		    x: -250,
 		    y: 130
-		});
+		})
+        .bind('menuanimation1', function () {
+            this.tween({
+                x: 50
+            }, 10).bind('TweenEnd', function () {
+                this.unbind('TweenEnd');
+                Crafty.trigger('menuanimation2');
+            })
+        });
 
-    Crafty.e('2D,Canvas,Button')
+
+    //Editor button
+    Crafty.e('2D,Canvas,Button,Tween')
 		.Button('Editor')
 		.attr({
-		    x: 50,
+		    x: -250,
 		    y: 210
-		});
+		})
+        .bind('menuanimation2', function () {
+            this.tween({
+                x: 50
+            }, 10).bind('TweenEnd', function () {
+                this.unbind('TweenEnd');
+                Crafty.trigger('menuanimation3');
+            })
+        });
 
-    Crafty.e('2D,Canvas,Button')
-    .Button('Settings')
-    .attr({
-        x: 50,
-        y: 290
-    });
 
-    Crafty.e('2D,Canvas,Button')
-    .Button('Credits')
-    .attr({
-        x: 50,
-        y: 370
-    });
+    //Settings button
+    Crafty.e('2D,Canvas,Button,Tween')
+        .Button('Settings')
+        .attr({
+            x: -250,
+            y: 290
+        })
+        .bind('menuanimation3', function () {
+            this.tween({
+                x: 50
+            }, 10).bind('TweenEnd', function () {
+                this.unbind('TweenEnd');
+                Crafty.trigger('menuanimation4');
+            })
+        });
+
+
+    //Credits button
+    Crafty.e('2D,Canvas,Button,Tween')
+        .Button('Credits')
+        .attr({
+            x: -250,
+            y: 370
+        })
+        .bind('menuanimation4', function () {
+            this.tween({
+                x: 50
+            }, 10)
+        });
+
+    Crafty.trigger('startMenuAnimation');
 });
