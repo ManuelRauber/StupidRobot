175de779724c9f0394a4226fafb87d614f2cf3f8
Manuel Rauber <github@manuel-rauber.de>
2013-10-08 16:57:47 +0200

    added stubs for settings & imprint command

--- StupidRobot/StupidRobot/js/main.js
+++ StupidRobot/StupidRobot/js/main.js
@@ -42,7 +42,17 @@
 			Windows.System.Launcher.launchUriAsync(new Windows.Foundation.Uri('http://cheeseware.de/datenschutzerklarung'));
 		});
 
+		var settingsCommand = new Windows.UI.ApplicationSettings.SettingsCommand('settings', 'Settings', function(command) {
+			// TODO: Open the settings page
+		});
+
+		var imprintCommand = new Windows.UI.ApplicationSettings.SettingsCommand('imprint', 'Imprint', function(command) {
+			// TODO: Open the imprint page
+		});
+		
 		e.request.applicationCommands.append(privacyPolicyCommand);
+		e.request.applicationCommands.append(settingsCommand);
+		e.request.ApplicationCommands.append(imprintCommand);
 	}
 
 	app.start();
