b5f5313ae031e76e0195e93e8784e48cd6996b7a
Manuel Rauber <github@manuel-rauber.de>
2013-10-13 16:35:29 +0200

    added preferences object

---
+++ StupidRobot/StupidRobot/js/preferences.js
@@ -0,0 +1,13 @@
+﻿Preferences = {	
+	isMusicEnabled: function() {
+		var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+
+		return roamingSettings.values["IsMusicEnabled"];
+	},
+	
+	setMusicEnabled: function(value) {
+		var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+
+		roamingSettings.values["IsMusicEnabled"] = value;
+	}
+};
