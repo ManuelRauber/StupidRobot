58c15c532ef9876528a5b4b8d1bb3c91d615ca0e
Timo Frank <Timo-Frank@gmx.net>
2013-10-14 22:46:20 +0200

    added first attempt for HTML Grid in Editor scene

--- StupidRobot/StupidRobot/js/scenes/Editor.js
+++ StupidRobot/StupidRobot/js/scenes/Editor.js
@@ -4,6 +4,32 @@
 	Crafty.scene(StupidRobot.Scenes.Editor, function() {
 		Crafty.background('green');
 
+		var width = StupidRobot.Game.width;
+		var height = StupidRobot.Game.height;
+
+		var htmlString = '<table width="100%" height="100%" cellspacing="0" cellpadding="0" border="1">';
+		for (var i=0; i < 30; i++)
+		{
+			htmlString += '<tr>';
+			for (var j = 0; j < 30; j++)
+			{
+				htmlString += '<td></td>';
+			}
+			htmlString += '</tr>';
+		}
+		htmlString += '</table>';
+
+
+		Crafty.e('HTML')
+		.attr({
+			x: 0,
+			y: 0,
+			w: width,
+			h: height
+		})
+		.replace(htmlString);
+			
+
 	
 	});
 })();
