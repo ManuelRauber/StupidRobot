471a9e6cbb3fd53312cb5b54bde78ed165c8711d
Timo Frank <Timo-Frank@gmx.net>
2013-10-14 23:46:06 +0200

    changed HTML Grid to Canvas Grid

---
+++ StupidRobot/StupidRobot/js/components/Grid.js
@@ -0,0 +1,31 @@
+﻿(function () {
+	Crafty.c('Grid', {
+		Grid: function (x, y, w, h, blocks) {
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+			this.blocks = blocks;
+
+			return this;
+		},
+
+		init: function () {
+			this.windowWidth = StupidRobot.Game.width;
+			this.windowHeight = StupidRobot.Game.height;
+		},
+
+		draw: function () {
+			var ctx = Crafty.canvas.context;
+			ctx.save();
+			
+			for (var j = 0; j < this.blocks; j++) {
+				for (var i = 0; i < this.blocks; i++) {
+					ctx.strokeRect(this.x + (this.w * i), this.y + (this.h * j), this.w, this.h);
+				}
+			}
+
+			ctx.restore();
+		}
+	});
+})();
--- StupidRobot/StupidRobot/js/scenes/Editor.js
+++ StupidRobot/StupidRobot/js/scenes/Editor.js
@@ -7,29 +7,10 @@
 		var width = StupidRobot.Game.width;
 		var height = StupidRobot.Game.height;
 
-		var htmlString = '<table width="100%" height="100%" cellspacing="0" cellpadding="0" border="1">';
-		for (var i=0; i < 30; i++)
-		{
-			htmlString += '<tr>';
-			for (var j = 0; j < 30; j++)
-			{
-				htmlString += '<td></td>';
-			}
-			htmlString += '</tr>';
-		}
-		htmlString += '</table>';
 
-
-		Crafty.e('HTML')
-		.attr({
-			x: 0,
-			y: 0,
-			w: width,
-			h: height
-		})
-		.replace(htmlString);
+		Crafty.e('2D, Canvas, Grid')
+			.Grid(0, 0, 50, 50, 20);
 			
-
 	
 	});
 })();
