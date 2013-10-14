2258f3add9049c9280aa26d263a11975d099665f
Manuel Rauber <github@manuel-rauber.de>
2013-10-14 21:52:11 +0200

    rewrite Preferences object to use WinJS.Class.define

--- StupidRobot/StupidRobot/js/preferences.js
+++ StupidRobot/StupidRobot/js/preferences.js
@@ -1,13 +1,37 @@
-﻿Preferences = {	
-	isMusicEnabled: function() {
-		var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+﻿(function() {
+	var settingKeys = {
+		IsMusicEnabled: "IsMusicEnabled"
+	};
 
-		return roamingSettings.values["IsMusicEnabled"];
-	},
-	
-	setMusicEnabled: function(value) {
-		var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+	var preferencesClass = WinJS.Class.define(
+		function() {
+			this._roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+		},
+		{			
+			_roamingSettings: null,
+			
+			isMusicEnabled: {
+				get: function () {
+					return this._roamingSettings.values[settingKeys.IsMusicEnabled];
+				},
+				set: function(value) {
+					if (typeof(value) != "boolean") {
+						throw "value is not boolean";
+					}
 
-		roamingSettings.values["IsMusicEnabled"] = value;
-	}
-};
+					this._roamingSettings.values[settingKeys.IsMusicEnabled] = value;
+				}
+			}
+		},
+		null);
+
+	var preferences = new preferencesClass();
+
+	WinJS.Namespace.define('StupidRobot', {		
+		Preferences: {
+			get: function() {
+				return preferences;
+			}
+		}
+	});
+})();
