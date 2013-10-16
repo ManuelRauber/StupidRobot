1fb49432e5b6ff6eac9f52e439257a414c47a38e
Manuel Rauber <github@manuel-rauber.de>
2013-10-16 09:22:08 +0200

    added a flyout showing the current music title

--- StupidRobot/StupidRobot/js/components/BackgroundMusicPlayer.js
+++ StupidRobot/StupidRobot/js/components/BackgroundMusicPlayer.js
@@ -1,16 +1,26 @@
 ﻿(function () {
 	var backgroundPlayer = WinJS.Class.define(
-		function () {
+		function() {
 			this._init();
 		},
 	{
-		_musicList: {
+		_currentPlayIndex: null,
+
+		musicList: {
 			get: function () {
-				return [
-					'/assets/Game/Sounds/Music/TheComplex.mp3',
-					'/assets/Game/Sounds/Music/DiscoConTutti.mp3',
-					'/assets/Game/Sounds/Music/BrightlyFancy.mp3'
-				];
+				return [{
+						title: 'The Complex',
+						interpret: 'Kevin MacLeod',
+						file: '/assets/Game/Sounds/Music/TheComplex.mp3'
+					}, {
+						title: 'Disco con Tutti',
+						interpret: 'Kevin MacLeod',
+						file: '/assets/Game/Sounds/Music/DiscoConTutti.mp3'
+					}, {
+						title: 'Brightly Fancy',
+						interpret: 'Kevin MacLeod',
+						file: '/assets/Game/Sounds/Music/BrightlyFancy.mp3'
+					}];
 			}
 		},
 
@@ -20,18 +30,32 @@
 		},
 
 		_init: function () {
-			for (var i = 0; i < this._musicList.length; i++) {
-				Crafty.audio.add('bgm' + i, this._musicList[i]);
+			for (var i = 0; i < this.musicList.length; i++) {
+				Crafty.audio.add('bgm' + i, this.musicList[i].file);
 			}
 		},
 
 		_play: function () {
-			var rnd = this._getRandomInt(0, this._musicList.length - 1);
+			var rnd = this._getRandomInt(0, this.musicList.length - 1);
 			Crafty.audio.play('bgm' + rnd);
+			this._currentPlayIndex = rnd;
+
+			this._showFlyout();
 
 			Crafty.audio.sounds['bgm' + rnd].obj.addEventListener('ended', function () {
 				StupidRobot.BackgroundMusicPlayer._play();
 			});
+		},
+		
+		_showFlyout: function() {
+			document.getElementById('backgroundMusicFlyoutTitle').textContent = this.musicList[this._currentPlayIndex].title;
+			document.getElementById('backgroundMusicFlyoutInterpret').textContent = this.musicList[this._currentPlayIndex].interpret;
+			document.getElementById('backgroundMusicFlyout').winControl.show('pageBody', 'bottom', 'right');
+			setTimeout(StupidRobot.BackgroundMusicPlayer._hideFlyout, 5000);
+		},
+		
+		_hideFlyout: function() {
+			document.getElementById('backgroundMusicFlyout').winControl.hide();
 		},
 
 		start: function () {
