ab51e6638311f7098d69f697f1f6ab4dd0e89596
Timo Frank <Timo-Frank@gmx.net>
2013-10-21 01:30:15 +0200

    added first attempt of appbar in editor

--- StupidRobot/StupidRobot/js/scenes/Editor.js
+++ StupidRobot/StupidRobot/js/scenes/Editor.js
@@ -6,6 +6,7 @@
 
 		var windowWidth = StupidRobot.Game.width;
 		var windowHeight = StupidRobot.Game.height;
+		var appBar;
 
 		Crafty.e('2D, Canvas, Grid')
 			.Grid(0, 0, windowWidth - 200, windowHeight, 20);
@@ -13,7 +14,8 @@
 		Crafty.e('2D, Canvas, EditorTaskbar')
 			.EditorTaskbar(windowWidth - 200, 0, 200, windowHeight);
 
-			
+		appBar = document.getElementById('appbar').winControl;
+		appBar.show();
 	
 	});
 })();
