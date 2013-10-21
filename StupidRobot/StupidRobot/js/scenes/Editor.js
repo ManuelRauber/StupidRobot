d3944c10e625449f761ece75d26793b8dc90be60
Timo Frank <Timo-Frank@gmx.net>
2013-10-21 15:50:11 +0200

    made top and bottom appbar with first buttons in editor

--- StupidRobot/StupidRobot/js/scenes/Editor.js
+++ StupidRobot/StupidRobot/js/scenes/Editor.js
@@ -6,7 +6,9 @@
 
 		var windowWidth = StupidRobot.Game.width;
 		var windowHeight = StupidRobot.Game.height;
-		var appBar;
+
+		var manageBar;
+		var editBar;
 
 		Crafty.e('2D, Canvas, Grid')
 			.Grid(0, 0, windowWidth - 200, windowHeight, 20);
@@ -14,8 +16,10 @@
 		Crafty.e('2D, Canvas, EditorTaskbar')
 			.EditorTaskbar(windowWidth - 200, 0, 200, windowHeight);
 
-		appBar = document.getElementById('appbar').winControl;
-		appBar.show();
+		manageBar = document.getElementById('editor_manageBar').winControl;
+		editBar = document.getElementById('editor_editBar').winControl;
+		manageBar.show();
+		editBar.show();
 	
 	});
 })();
