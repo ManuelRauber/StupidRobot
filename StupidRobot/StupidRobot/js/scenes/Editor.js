7c2c7a38d96d04a43268cb24499e67f253a008f2
Timo Frank <Timo-Frank@gmx.net>
2013-10-22 00:08:18 +0200

    added first attempt of backgroundimage for the Editor

--- StupidRobot/StupidRobot/js/scenes/Editor.js
+++ StupidRobot/StupidRobot/js/scenes/Editor.js
@@ -2,17 +2,24 @@
 	"use strict";
 
 	Crafty.scene(StupidRobot.Scenes.Editor, function() {
-		Crafty.background('green');
+		Crafty.background("url('/assets/Game/Images/Backgrounds/editor_Background.png')");
 
 		var windowWidth = StupidRobot.Game.width;
 		var windowHeight = StupidRobot.Game.height;
+		
+		/*
+		Crafty.e('2D, DOM, Image')
+			.attr({ w: windowWidth, h: windowHeight })
+			.css({
+				'background-image': 'url("/assets/Game/Images/Backgrounds/editor_Background.png")',
+				'background-repeat': 'no-repeat',
+				'background-size': '100% 100%',
+			});
+			*/
 
 		Crafty.e('2D, Canvas, Grid')
 			.Grid( (windowWidth / 2), (windowHeight / 2), null, null, 20);
 		
-		Crafty.e('2D, Canvas, EditorTaskbar')
-			.EditorTaskbar(windowWidth - 200, 0, 200, windowHeight);
-
 		var manageBar = document.getElementById('editor_manageBar').winControl;
 		var editBar = document.getElementById('editor_editBar').winControl;
 		manageBar.show();
