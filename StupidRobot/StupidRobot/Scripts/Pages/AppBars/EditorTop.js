53cfa52db8b725c2999288144931b3c94a7368b0
Timo Frank <Timo-Frank@gmx.net>
2013-10-22 18:41:06 +0200

    made new-flyout for editor + first template for save button

--- StupidRobot/StupidRobot/Scripts/Pages/AppBars/EditorTop.js
+++ StupidRobot/StupidRobot/Scripts/Pages/AppBars/EditorTop.js
@@ -6,7 +6,33 @@
 		null,
 		{
 			doSave: function () {
-				console.log("save button clicked!");
+				//TODO: pass the map object to this saving code
+
+				// Create the picker object and set options 
+				var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+				savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+				savePicker.fileTypeChoices.insert("Stupid Map", [".stupidmap"]);
+				savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+				savePicker.suggestedFileName = "map";
+
+				savePicker.pickSaveFileAsync().then(function (file) {
+					if (file) {
+						// Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync. 
+						Windows.Storage.CachedFileManager.deferUpdates(file);
+						// write to file 
+						Windows.Storage.FileIO.writeTextAsync(file, file.name).done(function () {
+							Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
+								if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
+									console.log("File " + file.name + " was saved.", "sample", "status");
+								} else {
+									console.log && WinJS.log("File " + file.name + " couldn't be saved.", "sample", "status");
+								}
+							});
+						});
+					} else {
+						console.log("Operation cancelled.", "sample", "status");
+					}
+				});
 			}
 	});
 
