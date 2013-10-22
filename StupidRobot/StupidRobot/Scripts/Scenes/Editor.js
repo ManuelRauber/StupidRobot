5f5fc9ea97484a6400cbde59727a87fead3e12a9
Manuel Rauber <github@manuel-rauber.de>
2013-10-22 23:07:03 +0200

    no classes around scenes

--- StupidRobot/StupidRobot/Scripts/Scenes/Editor.js
+++ StupidRobot/StupidRobot/Scripts/Scenes/Editor.js
@@ -1,31 +1,18 @@
-﻿(function () {
+﻿(function() {
 	"use strict";
 
-	var editorClass = WinJS.Class.define(
-		function () {
-			Crafty.scene(StupidRobot.Scenes.Editor, function () {
-				Crafty.background("url('/assets/Game/Images/Backgrounds/EditorBackground.png')");
+	Crafty.scene(StupidRobot.Scenes.Editor, function() {
+		Crafty.background("url('/assets/Game/Images/Backgrounds/EditorBackground.png')");
 
-				var windowWidth = StupidRobot.Game.width;
-				var windowHeight = StupidRobot.Game.height;
+		var windowWidth = StupidRobot.Game.width;
+		var windowHeight = StupidRobot.Game.height;
 
-				Crafty.e('2D, Canvas, Grid')
-					.Grid((windowWidth / 2), (windowHeight / 2), null, null, 20);
+		Crafty.e('2D, Canvas, Grid')
+			.Grid((windowWidth / 2), (windowHeight / 2), null, null, 20);
 
-				var manageBar = document.getElementById('EditorManageBar').winControl;
-				var editBar = document.getElementById('EditorEditBar').winControl;
-				manageBar.show();
-				editBar.show();
-			});
-		},
-		null,
-		{
-		}
-	);
-
-	var editor = new editorClass();
-
-	WinJS.Namespace.define('StupidRobot', {
-		Editor: editor
+		var manageBar = document.getElementById('EditorManageBar').winControl;
+		var editBar = document.getElementById('EditorEditBar').winControl;
+		manageBar.show();
+		editBar.show();
 	});
 })();
