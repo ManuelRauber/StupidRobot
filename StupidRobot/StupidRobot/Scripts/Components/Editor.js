f73e8db157a3d768f7095c75927b4b4de4b9da76
Timo Frank <Timo-Frank@gmx.net>
2013-10-24 20:15:15 +0200

    added mapname & mapsize creation

--- StupidRobot/StupidRobot/Scripts/Components/Editor.js
+++ StupidRobot/StupidRobot/Scripts/Components/Editor.js
@@ -25,20 +25,24 @@
 
 		_createMap: function (data) {
 
+			//get mapname and mapsize of data
 			var mapname = data['mapname'].value;
+			var gridblocks = { 'small': 10, 'medium': 15, 'big': 20 };
 			var size;
 			for (var i = 0 ; i < data['mapsize'].length; i++) {
 				if (data['mapsize'][i].checked) {
-					size = data['mapsize'][i].value;
+					size = gridblocks[ data['mapsize'][i].value ];
 				}
 			}
 
 			console.log(mapname);
 			console.log(size);
 
-
+			//create grid
 			this._grid = Crafty.e('2D, Canvas, Grid')
-				.Grid((this.windowWidth / 2), (this.windowHeight / 2), null, null, 20);
+				.Grid((this.windowWidth / 2), (this.windowHeight / 2), null, null, size);
+
+			//force the component to redraw
 			this.trigger('Change');
 
 		}
