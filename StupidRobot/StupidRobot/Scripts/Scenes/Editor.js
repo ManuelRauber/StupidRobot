98b0ef945df40c10390992879360ebc0b571e04f
Timo Frank <Timo-Frank@gmx.net>
2013-10-24 22:08:25 +0200

    added WinJS.ui.Listview for entities in Editor

--- StupidRobot/StupidRobot/Scripts/Scenes/Editor.js
+++ StupidRobot/StupidRobot/Scripts/Scenes/Editor.js
@@ -20,4 +20,43 @@
 		manageBar.show();
 		editBar.show();
 	});
+
+
+	var entities = WinJS.Class.define(
+		null,
+		{
+			setEntities: function () {
+				var dataArray = [
+					{ title: "Basic banana", text: "Low-fat frozen yogurt", picture: "images/60banana.png" },
+					{ title: "Banana blast", text: "Ice cream", picture: "images/60banana.png" },
+					{ title: "Brilliant banana", text: "Frozen custard", picture: "images/60banana.png" },
+					{ title: "Orange surprise", text: "Sherbet", picture: "images/60orange.png" },
+					{ title: "Original orange", text: "Sherbet", picture: "images/60orange.png" },
+					{ title: "Vanilla", text: "Ice cream", picture: "images/60vanilla.png" },
+					{ title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
+					{ title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" },
+					{ title: "Succulent strawberry", text: "Sorbet", picture: "images/60strawberry.png" }
+				];
+				this.itemList = new WinJS.Binding.List(dataArray);
+			},
+
+			PublicMembers: {
+				get: function () {
+					return this.itemList;
+				}
+			}
+		}
+	);
+
+	var editorEntities = new entities();
+	editorEntities.setEntities();
+	
+
+	WinJS.Namespace.define("StupidRobot.Editor", {
+		Entities: {
+			get: function () {
+				return editorEntities.PublicMembers;
+			}
+		}
+	});
 })();
