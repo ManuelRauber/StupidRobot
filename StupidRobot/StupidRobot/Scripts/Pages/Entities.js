c30b9c9928133087f6428dd81a84fcd77864898e
Timo Frank <Timo-Frank@gmx.net>
2013-11-03 22:51:03 +0100

    made new Entity structure

--- StupidRobot/StupidRobot/Scripts/Pages/Entities.js
+++ StupidRobot/StupidRobot/Scripts/Pages/Entities.js
@@ -6,28 +6,26 @@
 		{
 			init: function () {
 				//load theme forest as standard
-				this.loadEntities('forest');
+				this.loadEntities('desert');
 			},
 
 			loadEntities: function (theme) {
 				var themes = [];
 				themes['forest'] = [
-					{ type: "solid[0]", title: "Tree", text: "a nice tree", picture: "/Assets/Game/Images/tree.scale-100.png" },
-					{ type: "solid[1]", title: "Rocket", text: "yipiiii", picture: "/Assets/Game/Images/rocket.scale-100.png" },
-					{ type: "solid[2]", title: "Robot", text: "south", picture: "/Assets/Game/Images/robot-south.scale-100.png" },
-					{ type: "free[0]", title: "Robot", text: "west", picture: "/Assets/Game/Images/robot-west.scale-100.png" },
+					{ type: "solid[0]", title: "Tree", text: "a nice tree", picture: "/Assets/Game/Images/Entities/Landscape/Forest/forest_wall.png" },
+					
 				];
 				themes['desert'] = [
-					{ type: "solid[0]", title: "wall", text: "brick wall", picture: "/Assets/Game/Images/Entities/desert_wall.png" },
-					{ type: "solid[1]", title: "rock", text: "solid rock", picture: "/Assets/Game/Images/Entities/desert_rock.png" },
-					{ type: "solid[2]", title: "bush", text: "a normal bush", picture: "/Assets/Game/Images/Entities/desert_bush.png" },
-					{ type: "free[0]", title: "ground", text: "ground", picture: "/Assets/Game/Images/Entities/desert_ground.png" },
+					{ type: "solid[0]", title: "wall", text: "brick wall", picture: "/Assets/Game/Images/Entities/Landscape/Desert/desert_wall.png" },
+					{ type: "solid[1]", title: "rock", text: "solid rock", picture: "/Assets/Game/Images/Entities/Landscape/Desert/desert_rock.png" },
+					{ type: "solid[2]", title: "bush", text: "a normal bush", picture: "/Assets/Game/Images/Entities/Landscape/Desert/desert_bush.png" },
+					{ type: "free[0]", title: "ground", text: "ground", picture: "/Assets/Game/Images/Entities/Landscape/Desert/desert_ground.png" },
 				];
 				themes['dungeon'] = [
-					{ type: "solid[0]", title: "wall", text: "dungeon wall", picture: "/Assets/Game/Images/Entities/dungeon_wall.png" },
-					{ type: "solid[1]", title: "hole", text: "huge hole", picture: "/Assets/Game/Images/Entities/dungeon_hole.png" },
-					{ type: "solid[2]", title: "pipe", text: "super mario?", picture: "/Assets/Game/Images/Entities/dungeon_pipe.png" },
-					{ type: "free[0]", title: "ground", text: "dungeon floor", picture: "/Assets/Game/Images/Entities/dungeon_ground.png" },
+					{ type: "solid[0]", title: "wall", text: "dungeon wall", picture: "/Assets/Game/Images/Entities/Landscape/Dungeon/dungeon_wall.png" },
+					{ type: "solid[1]", title: "hole", text: "huge hole", picture: "/Assets/Game/Images/Entities/Landscape/Dungeon/dungeon_hole.png" },
+					{ type: "solid[2]", title: "pipe", text: "super mario?", picture: "/Assets/Game/Images/Entities/Landscape/Dungeon/dungeon_pipe.png" },
+					{ type: "free[0]", title: "ground", text: "dungeon floor", picture: "/Assets/Game/Images/Entities/Landscape/Dungeon/dungeon_ground.png" },
 				];
 				var dataArray = themes[theme];
 				this.themes = themes;
